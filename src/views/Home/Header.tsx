eafb0c540d8e8f5071fa87a90153108491d98956
xiaolong <longjiuwei999@163.com>
2023-08-17 15:23:11 +0800

    modify header css

--- src/views/Home/Header.tsx
+++ src/views/Home/Header.tsx
@@ -4,14 +4,14 @@ export default function Header() {
     return <div className="h-5 w-full flex justify-between items-center">
         <div className="h-full flex items-center  p-4 rounded-xl bg-dark-200 cursor-pointer hover:bg-dark-100">
             <span className="text-white mr-1">Account 1</span>
-            <CaretDownOutlined color="#fff" />
+            <CaretDownOutlined className=" text-white" />
         </div>
         <div className="h-full flex items-center">
             <div className="h-full flex items-center mr-2 p-4 rounded-xl bg-dark-200 cursor-pointer hover:bg-dark-100">
                 <span className="text-white mr-2">Mainnet</span>
                 <span className="w-2 h-2 bg-green rounded-full"></span>
             </div>
-            <SettingOutlined color="#848587" className="text-2xl cursor-pointer hover:text-white" />
+            <SettingOutlined className="text-[#848587] text-2xl cursor-pointer hover:text-white" />
         </div>
     </div>
 }
