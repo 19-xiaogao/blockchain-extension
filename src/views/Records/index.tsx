c7914371b33734f34ba389906c5327e718caf0d1
xiaolong <longjiuwei999@163.com>
2023-08-21 14:05:08 +0800

    writed record view

--- src/views/Records/index.tsx
+++ src/views/Records/index.tsx
@@ -1,6 +1,44 @@
+import { SwapOutlined } from "@ant-design/icons";
 import React from "react";
+import { formatAddress } from "~utils";
 
 export default function WalletView() {
 
-    return <div> this si wallet view</div>
+    return <div className="p-4">
+        <div className=" text-center mt-2">
+            <span className=" text-2xl text-white font-semibold">Activity</span>
+        </div>
+        <div className="mt-4">
+            <div className="rounded-lg p-3 bg-[#1d1f22] flex items-center justify-between cursor-pointer">
+                <div className="flex items-center">
+                    <SwapOutlined className="text-white text-2xl" />
+                    <div className="ml-2">
+                        <div className="text-white text-base">Receive</div>
+                        <div className="text-[#838385]">From: {formatAddress("0xAa5A88bdA5BB06cb73Ee0af753D3f4A2486dd845")}</div>
+                    </div>
+                </div>
+                <div className="text-[#47977a] text-base font-semibold">+0.012 ETH</div>
+            </div>
+            <div className="mt-3 rounded-lg p-3 bg-[#1d1f22] flex items-center justify-between cursor-pointer">
+                <div className="flex items-center">
+                    <SwapOutlined className="text-white text-2xl" />
+                    <div className="ml-2">
+                        <div className="text-white text-base">Receive</div>
+                        <div className="text-[#838385]">From: {formatAddress("0xAa5A88bdA5BB06cb73Ee0af753D3f4A2486dd845")}</div>
+                    </div>
+                </div>
+                <div className="text-[#47977a] text-base font-semibold">+0.012 ETH</div>
+            </div>
+            <div className="mt-3 rounded-lg p-3 bg-[#1d1f22] flex items-center justify-between cursor-pointer">
+                <div className="flex items-center">
+                    <SwapOutlined className="text-white text-2xl" />
+                    <div className="ml-2">
+                        <div className="text-white text-base">Receive</div>
+                        <div className="text-[#838385]">From: {formatAddress("0xAa5A88bdA5BB06cb73Ee0af753D3f4A2486dd845")}</div>
+                    </div>
+                </div>
+                <div className="text-[#47977a] text-base font-semibold">+0.012 ETH</div>
+            </div>
+        </div>
+    </div>
 }
