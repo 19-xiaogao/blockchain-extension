c797f7df98f2950b0cfa206123b03a23a99be18c
xiaolong <longjiuwei999@163.com>
2023-08-22 10:33:13 +0800

    writed setting view

--- src/views/Setting/index.tsx
+++ src/views/Setting/index.tsx
@@ -18,7 +18,7 @@ export default function Setting() {
             }
             right={<CloseOutlined className=" text-[#9f9fa1] text-xl  cursor-pointer" onClick={() => navigate("/")} />}
             onBack={back}>
-            <span className="text-white text-base">Add tokens</span>
+            <span className="text-white text-base">Settings</span>
         </NavBar>
 
     </div>
