0ffdea2f29b739f09d6c0f4b875d3981aea48129
xiaolong <longjiuwei999@163.com>
2023-09-11 16:16:16 +0800

    writed setting view

--- src/views/Setting/index.tsx
+++ src/views/Setting/index.tsx
@@ -2,18 +2,22 @@ import { ArrowLeftOutlined, CloseOutlined, LockFilled } from "@ant-design/icons"
 import { NavBar } from "antd-mobile";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import { removeStoragePassword } from '~background'
 export default function Setting() {
     const navigate = useNavigate()
     const back = () => {
         console.log("click back icon")
+    }
+    const handleLockClick = async () => {
+        await removeStoragePassword()
+        navigate('/lock')
     }
     return <div>
         <NavBar
             backArrow={
                 <LockFilled
                     className=" text-[#9f9fa1] text-xl mb-2"
-                    onClick={() => navigate('/lock')}
+                    onClick={handleLockClick}
                 />
             }
             right={<CloseOutlined className=" text-[#9f9fa1] text-xl  cursor-pointer" onClick={() => navigate("/")} />}
